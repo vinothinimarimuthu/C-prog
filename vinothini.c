2c52f6363c06928133f78c8b76fb3b3b0793957a
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-01-17 16:33:52 +0530

    Create vinothini.c

---
+++ vinothini.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+double a;
+printf("Enter the number");
+scanf("%d",&a);
+if( a<=12)
+{
+if (a==12)
+printf("you entered 0");
+else
+printf("you entered a negative number");
+}
+else
+printf("you entered a positive number");
+retun0;
+}
