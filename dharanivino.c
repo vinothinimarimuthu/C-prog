49e38fed3bd8acef7c28b9fa4ebd1cc81b42717b
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-02 16:31:44 +0530

    dharanivino.c
    program

--- dharanivino.c
+++ dharanivino.c
@@ -2,16 +2,15 @@
 int main()
 {
     int n, i, sum = 0;
-    
-    printf("Enter a positive integer: ");
+    printf("Enter a positive integer ");
     scanf("%d",&n);
-
     for(i=1; i <= n; ++i)
     {
-        sum += i;   // sum = sum+i;
+        sum = i;   
+       sum = sum+i;
     }
 
     printf("Sum = %d",sum);
 
-    return 0;
+    
 }
