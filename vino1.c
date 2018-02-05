95e1e0ca616680a652da1af5e1ad4338badb1e9b
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-05 15:45:02 +0530

    vino1.c
    program

---
+++ vino1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the a,b,c value);
+scanf("%d%d&a&b&c");
+if(a>b>c)
+{
+printf("\n %d a is positive number%d&a&b&c");
+else
+printf("\n %d a is negative number%d&a&b&c");
+else
+printf("\n %d a is zero%d&a&b&c");
+}
