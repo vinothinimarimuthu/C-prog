2f11a4f7787d3971a809e886ec0d774340c27e11
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-05 16:47:09 +0530

    vino7.c
    program

---
+++ vino7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("Enter the a,b value");
+scanf("%d %d&a&b");
+if(a>b)
+{
+printf("\n a is even number ");
+else
+printf(" a is odd number ");
+printf("odd numbers between two intervals");
+
+}
