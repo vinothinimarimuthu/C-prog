4d03092e5f2bf9d6650776a62f33dc665eec1239
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-07 15:57:35 +0530

    palindrome.c
    program

---
+++ palindrome.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num,count=0;
+printf("Enter the number");
+scanf("%d",&num);
+while(num!=0)
+{
+digit=num%10
+num=num%10
+num=num/10;
+reverse=reverse*10+digits
+{
+printf("reverse of digits %d",&reverse);
+}
