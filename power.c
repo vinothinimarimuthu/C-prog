1f92e614cccacd45e5211d603b668565c7add26d
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-07 16:06:03 +0530

    power.c
    program

---
+++ power.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int base,exponent ;
+printf("Enter a base number: ");
+scanf("%d", &base);
+printf("Enter an exponent: ");
+scanf("%d", &exponent);
+while (exponent != 0)
+ {
+    result *= base;
+     --exponent;
+    }
+    printf("Answer = %lld", result);
+    return 0;
+}
