70b38101fe0efea9227bf611f90adbec48333e27
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-09 16:41:19 +0530

    countn.c
    count the number of words

---
+++ countn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h> 
+void main()
+{
+    char s[200];
+    int count = 0, i;
+    scanf("%d",& a);
+    for (i = 0;a[i] != '\0';i++)
+    {
+        if (a[i] == ' ')
+            count++;    
+    }
+    printf("%d", count + 1);
+}
