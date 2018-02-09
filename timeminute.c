d215b5209671a3cfbf73ed7620ef5a531531505d
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-09 16:20:36 +0530

    timeminute.c
    program

---
+++ timeminute.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main()
+{
+      int hours, minute;
+      printf("Enter the Time in Hours and Minutes\n");
+      printf("\nHours");
+      scanf("%d", &hours);
+      printf("\nMinutes:\t");
+      scanf("%d", &minutes);
+      printf("\nTime and Minutes:\t%d\n");
+      
+}
