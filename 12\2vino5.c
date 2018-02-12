6b19f3052e1eac88a650e4d736814799b1e8420b
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-12 16:30:06 +0530

    12\2vino5.c
    program

---
+++ 12\2vino5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int x;int  y;
+x=150;
+y=160;
+printf("\n before swapping%d%d&x/t&y");
+swap(x,y);
+getch():
+}swap(a,b)
+int=temp;
+temp=a;
+a=b;
+b=temp;
+printf("after swapping %d%d&x/t&y");
+}
