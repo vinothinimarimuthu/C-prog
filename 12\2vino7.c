852a324d1107e870aeb603260eed7341cc90c6ec
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-12 16:54:22 +0530

    12\2vino7.c
    program

--- 12\2vino7.c
+++ 12\2vino7.c
@@ -1,16 +1,15 @@
 #include<stdio.h>        
-int main()
+void main()
 {
-    int n,i;
+    int a,i;
     int f1=1,f2=1,f3;
     scanf("%d",&n);
     printf("%d  %d  ",f1,f2);
-    for(i=2;i<n;i++)
+    for(i=2;i<a;i++)
     {
         f3=f1+f2;
         printf("%d  ",f3);
         f1=f2;
         f2=f3;
     }
-    return 0;
 }
