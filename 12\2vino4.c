a3267117b38bdd5020a00da8e62c8495d4d7633c
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-12 16:27:10 +0530

    12\2vino4.c
    swapping of two numbers

---
+++ 12\2vino4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int x;int  y;
+x=10;
+y=20;
+printf("\n before swapping%d%d&x/t&y");
+swap(x,y);
+getch():
+}swap(a,b)
+int=temp;
+temp=a;
+a=b;
+b=temp;
+printf("after swapping %d%d&x/t&y");
