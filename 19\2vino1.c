b2a4d4f7a9dc482fc47a75c7a5edf6df1b2e7f59
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-19 16:03:34 +0530

    19\2vino1.c
    write a program using string

---
+++ 19\2vino1.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s;
+int k,i;
+printf("enter the string\n");
+scanf("%d",&s);
+printf("enter the number\n");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+	printf("%d",&k);
+}
+return 0;
+}
