0c93f67d52faafdb41b4261e90040e5c3f6b7031
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-19 16:46:14 +0530

    19\2vino4.c
    sum of two numbers

---
+++ 19\2vino4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the number 1:");
+scanf("%d",&a);
+printf("enter the number2:");
+scanf("%d",&b);
+sum=a+b;
+{
+printf("%d",&sum);
+}
+return 0;
+}
