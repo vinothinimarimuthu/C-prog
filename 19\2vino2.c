17fe0c3727b6066f8a12fceab7535425db7a0293
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-19 16:21:42 +0530

    19\2vino2.c
    string comparesion program without using strcmp()

---
+++ 19\2vino2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str1[10],str[20];
+	int i;
+	printf("enter the string 1");
+	scanf("%d",&str1);
+	printf("enter the string 2");
+	scanf("%d,&str2);
+	n1=strlen(str1);
+	n2=strlen(str2);
+	if(n1>n2)
+	{
+		printf("%d"&str1);
+	}
+	else if(n2>n1)
+	{
+		printf("%d",str2);
+	}
+	else
+	{
+		printf("%d",str2);
+	}
+	return 0;
+}
+	}
+	}
+
+}
