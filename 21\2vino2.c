e05f7649357c02061d8bee8ef0767df77ca6f900
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-21 16:25:00 +0530

    21\2vino2.c
    program for power of 2

---
+++ 21\2vino2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int temp,flag,num;
+printf("\n enter the number:");
+scanf("%d",&num);
+temp=num
+while(temp!=1)
+{
+if(temp2!=0)
+{
+flag=1;
+break;
+}
+if(flag==0)
+{
+printf("\n is power of 2,num");
+}
+else if(flag==1)
+{
+printf("\n is power of not 2,num");
+}
+return 0;
+}
+}
