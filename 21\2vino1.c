848ea71eb08783bf20879bce3a72bc296253da7d
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-21 15:57:06 +0530

    21\2vino1.c
    program for maximum number  and minimum number

---
+++ 21\2vino1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,n,i;
+printf("\n  enter the maximum number :");
+scanf("%d",&a);
+printf("\n enter the minimum number:");
+scanf("%d",&b);
+for(i=0;i<=10;i++)
+{
+if(i>n)
+{
+printf("%d",&a);
+}
+}
+{
+if(i<n)
+{
+printf("%d",&b);
+}
+}
+
