08a10bbfceb91bd690f1dc617d12b5d15fc72263
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-23 14:07:16 +0330

    23\2vino1.c
    program for average of n numbers

---
+++ 23\2vino1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i,avg;
+printf("enter the number\n:");
+scanf("%d",&n);
+for(i=0;i<=10;i++)
+{
+printf("%d",&n);
+}
+{
+avg=n1+n2+n3/3
+{
+printf("%d",&avg);
+}
+}
