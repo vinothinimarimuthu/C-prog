26824200b3833bd0ce5b3474735cfc1d4a5c49d0
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-23 14:37:45 +0330

    23\2vino2.c
    program for ranges between the numbers

---
+++ 23\2vino2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the number");
+scanf("%d",&n);
+if(n<10)
+{
+printf("numbers between the ranges");
+}
+else
+{
+printf("not print the ranges");
+}
+}
