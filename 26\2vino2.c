9691f74fa9defb2627a08ab7ad901e209edc13c1
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-26 03:17:57 -0800

    26\2vino2.c
    program for concodinates the string

--- 26\2vino2.c
+++ 26\2vino2.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-int num,count=0;
-printf("enter the n numbers:");
-scanf("%d",&num);
-while(num!=0)
-{
-digits=num%10;
-num=num%10;
-reverse=reverse*10+digits;
-{
-printf("\n%d",&reverse);
-}
-}
+int n;
+printf("enter the numbers:");
+  scanf("%s",str);
+  str1=strcat(str);
+  printf("the concodinate string %s",str1);
 }
