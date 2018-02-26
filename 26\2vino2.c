2487eee9aa42ff48322a27aae5e732ad6e661416
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-26 02:53:36 -0800

    26\2vino2.c
    program for reverse numbers

---
+++ 26\2vino2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num,count=0;
+printf("enter the n numbers:");
+scanf("%d",&num);
+while(num!=0)
+{
+digits=num%10;
+num=num%10;
+reverse=reverse*10+digits;
+{
+printf("\n%d",&reverse);
+}
+}
+}
