318afa903ae9515ec960bc809b40d1bd34527035
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-28 15:41:32 +0530

    3\1vino1.c

---
+++ 3\1vino1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the aand b values");
+scanf("%d"&a&b);
+if(a>b)
+{
+printf("\n%d is greater then %d,&a&b);
+}
+else
+{
+printf("\n%d is less then %d,&a&b);
+}
+}
