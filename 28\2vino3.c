6cd47f2846d660cb1a433817a0de3f2fca8f90eb
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-28 16:53:37 +0530

    28\2vino3.c
    program

---
+++ 28\2vino3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,a,i;
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("the sum of n numbers is%d",sum);
+}
+if(a%2==0)
+{
+printf("\n the given number %d is even",a);
+}
+else
+{
+printf("\n the given number %d is odd",a);
+}
+getch() 
+}
