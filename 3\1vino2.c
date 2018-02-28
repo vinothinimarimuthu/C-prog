12b01c9f80dca77754d719b83f5eea4339bfbcac
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-02-28 16:01:06 +0530

    3\1vino2.c
    program for swapping of two number

---
+++ 3\1vino2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(int*,int)
+void main()
+{
+int x,y;
+printf("enter two number");
+scanf("\t\t%d%d",&x,&y);
+add(x,y);
+}
+void(int a,int b)
+{
+int c=a+b;
+printf("\t\t the value is%d",c);
+}
