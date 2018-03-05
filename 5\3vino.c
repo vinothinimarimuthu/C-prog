eb9a5b5c705ee471772160f18f840da945fe9ed4
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-05 02:49:35 -0800

    5\3vino4.c
    program for give number and print number in word

---
+++ 5\3vino.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the 1to 10 number');
+scanf("%d",&num);
+switch(num)
+{
+case1:
+printf("\none");
+break;
+cse2:
+printf("\ntwo");
+break;
+case3:
+printf("\nthree");
+break;
+case4:
+printf("\nfour");
+break;
+case5:
+printf("\nfive");
+break;
+case6:
+printf(\nsix");
+break;
+case7:
+printf("\nseven");
+break;
+case8:
+printf("\neight");
+break;
+case9:
+printf("\nnine");
+break;
+case10:
+printf("\nten");
+break;
+default:
+printf("enter the 1 to 10 numbers");
+}
+return 0;
+}
