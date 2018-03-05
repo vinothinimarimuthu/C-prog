e93e5089739c64cba34027aee4f59def42580104
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-05 02:11:29 -0800

    5\3vino1.c
    program for add the two number is odd or even

---
+++ 5\3vino1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,n,i;
+for(i=0;i<=10;i++)
+{
+sum=sum+i;
+}
+{
+printf("sumof the number is");
+}
+if(a%2==0)
+{
+printf("\ngiven number is even");
+}
+else
+{
+printf("\ngiven number is odd");
+}
