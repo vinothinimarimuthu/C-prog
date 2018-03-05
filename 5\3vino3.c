621f5625b10d27712aa468d64cd2fe2067b52f25
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-05 02:28:52 -0800

    5\3vino3.c
    program for difference between the two number is odd or even

---
+++ 5\3vino3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,a,i;
+for(i=0;i<=10;i++)
+{
+differnce=differnce-i;
+}
+{
+printf("the differnce of n number");
+}
+if(a%2==0)
+{
+printf("\ngiven number %d is even");
+}
+else
+{
+printf("\ngiven number %d is odd");
+}
