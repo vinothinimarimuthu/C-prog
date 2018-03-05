c934297e57ad92b197f6aebd9450492e192cf309
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-05 03:04:35 -0800

    5\3vino5.c
    program  for give number nearest number isless then or equal to even number

---
+++ 5\3vino5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,a,i;
+printf("enter the n value");
+scanf("%d",n);
+if(n%2!==0)
+a=n-1;
+if(a%2==0)
+{
+printf("is value is equal to n");
+}
+else
+printf("is value is less then n");
+}
