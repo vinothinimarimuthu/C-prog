98f204f3a38e23290b08acb2b5e740c7527b8275
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-07 03:08:13 -0800

    7\3vino2.c
    program for given number is prime or not if it is prime print yes otherwise print no

--- 7\3vino2.c
+++ 7\3vino2.c
@@ -4,6 +4,15 @@ int main()
 	int a;
 	printf("enter the number");
 	scanf("%d",&a);
+	for(i=0;i<a/2;i++)
+	{
+        if(a%i=0)
+	{
+		flag=1;
+		break;
+	}
+		
+	}
 	if(a%2==0)
 	{
 		printf("\n given number is %d prime");
