6c3db9de150d84d0fb3d345813805bf16b26d854
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-09 03:10:20 -0800

    multiply13.c
    program for given number is multiple into 13

---
+++ multiply13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+	int a,n;
+	printf("enter the number");
+	scanf("%d",&n);
+	if(a%==0)
+	printf("given number is multiply 13");
+	else
+    printf("given number is not multiply 13");
+    return 0;
+}
