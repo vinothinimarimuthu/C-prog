6bbc0a6c269e10d13c3abaf566b743e61520033c
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-09 02:49:50 -0800

    multiply7.c
    program for given number is multiply into7

---
+++ multiply7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+	int a,n;
+	printf("enter the number");
+	scanf("%d",&n);
+	if(a%==0)
+	printf("given number is multiply 7");
+	else
+    printf("given number is not multiply 7");
+    return 0;
+}
