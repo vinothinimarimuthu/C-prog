06725fce611f09714bfaad5b9e7fc7bb6bc9f6d8
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-19 16:25:01 +0530

    19\3vino1.c
    program

---
+++ 19\3vino1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int base,exponent ;
+printf("Enter a base number ");
+scanf("%d", &base);
+printf("Enter an exponent");
+scanf("%d", &exponent);
+while (exponent != 0)
+ {
+    result *= base;
+     --exponent;
+    }
+    printf("Answer = %lld", result);
+    
+}
